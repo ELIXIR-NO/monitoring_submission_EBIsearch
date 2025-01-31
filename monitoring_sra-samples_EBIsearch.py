3d0676e8e496c0b3161d5e4094bbb1ef9a6560dd
Federico Bianchini <72258479+bianchini88@users.noreply.github.com>
2025-01-31 23:02:30 +0100

    Update monitoring_sra-samples_EBIsearch.py
    moving standardise centre to a function

--- monitoring_sra-samples_EBIsearch.py
+++ monitoring_sra-samples_EBIsearch.py
@@ -31,7 +31,89 @@ def get_entries(n_size,n_page,fields):
     if len(entries)!=get_dataset_size():
         print("WARNING: wrong number of entries: ", len(entries)," vs ", get_dataset_size(), " hits")
     return(entries)
+def standardise_centre(df):
+    UiO_list = [
+        'University of Oslo',
+        'UNIVERSITY OF OSLO',
+        'University of Oslo, Department of Biosciences',
+        'University of oslo',
+        'Universitetet i Oslo',
+        'Department of Paediatric Medicine, Oslo University Hospital',
+        'Oslo University',
+        'Unversity of Oslo',
+        'Oslo University Hospital',
+        'Insititute of Oral Biology, Univeristy of Oslo',
+        'Oslo University Hospital, Rikshopitalet',
+        'Archaeogenomics group, Department of Biosciences, University of Oslo',
+        'Oslo University Hospital/University of Oslo',
+        'OSLO UNIVERSITY HOSPITAL',
+        'Department of Biosciences, University of Oslo',
+        'Natural History Museum, University of Oslo',
+        'UIO',
+        'CEES'
+    ]
 
+    NTNU_list = [
+        'NTNU',
+        'NTNU - Norwegian University of Science and Technology',
+        'NTNU University Museum'
+    ]
+
+    UiB_list = [
+        'University of Bergen',
+        'CENTRE FOR GEOBIOLOGY, DEPARTMENT OF BIOLOGY, UNIVERSITY OF BERGEN, NORWAY',
+        'Universitetet i Bergen',
+        'UNIVERSITY OF BERGEN/DEPT. OF BIOLOGY',
+        'K.G. Jebsen center for deep-sea research, University of Bergen',
+        'Center for Geobiology, University of Bergen',
+        'UNIVERSITY OF BERGEN',
+        'UiB'
+    ]
+
+    NMBU_list = [
+        'FACULTY OF CHEMISTRY, BIOTECHNOLOGY AND FOOD SCIENCE (IKBM), NORWEGIAN UNIVERSITY OF LIFE SCIENCES (NMBU), NORWAY',
+        'NMBU Norwegian University of Life Sciences',
+        'NMBU',
+        'Norwegian University of Life Sciences (NMBU)',
+    ]
+
+    UiT_list = [
+        'University of Tromso',
+        'UiT the Arctic University of Norway',
+        'UiT: The Arctic University of Norway',
+        'UiT, The Arctic University of Norway',
+        'UIT',
+        'UiT The Arctic University if Norway',
+        'UiT The Arctic University',
+        'UiT - The Arctic University of Norway',
+        'The Arctic University of Norway',
+        'University of Tromsoe',
+        'Sletvold H., Department of Pharmacy, University of Tromso, Tromso, N-9037, NORWAY',
+        'Tromsoe University Museum',
+        'University of Tromso - The Arctic University of Norway',
+        'UNIVERSITY OF TROMSO'
+    ]
+
+    FHI_list=[
+        'Norwegian Institute of Public Health (NIPH)',
+        'NIPH',
+        'NORWEGIAN INSTITUTE OF PUBLIC HEALTH',
+        'Folkehelseinstituttet (FHI), Norway'
+    ]
+
+    centres = {
+        "UiO": UiO_list,
+        "UiB": UiB_list,
+        "UiT": UiT_list,
+        "NMBU": NMBU_list,
+        "NTNU": NTNU_list,
+        "FHI": FHI_list
+    }
+
+    for centre in centres.keys(): 
+        df.loc[df["center_name"].isin(centres[centre]), "center_name"] = centre
+    return df
+    
 # get all the data
 points_perpage = 1000 # max allowed by API
 n_pages = get_dataset_size()//points_perpage # pages to get the whole database
@@ -45,88 +127,6 @@ for column in fields.columns: # change from list to standard variable
     fields[column]=fields[column].apply(lambda x: x[0] if len(x) > 0 else None)
 # change to proper date/time type 
 fields["last_updated_date"] = fields["last_updated_date"].astype("datetime64[ns]")
-
-# making the centre name smoother
-UiO_list = [
-    'University of Oslo',
-    'UNIVERSITY OF OSLO',
-    'University of Oslo, Department of Biosciences',
-    'University of oslo',
-    'Universitetet i Oslo',
-    'Department of Paediatric Medicine, Oslo University Hospital',
-    'Oslo University',
-    'Unversity of Oslo',
-    'Oslo University Hospital',
-    'Insititute of Oral Biology, Univeristy of Oslo',
-    'Oslo University Hospital, Rikshopitalet',
-    'Archaeogenomics group, Department of Biosciences, University of Oslo',
-    'Oslo University Hospital/University of Oslo',
-    'OSLO UNIVERSITY HOSPITAL',
-    'Department of Biosciences, University of Oslo',
-    'Natural History Museum, University of Oslo',
-    'UIO',
-    'CEES'
-]
-
-NTNU_list = [
-    'NTNU',
-    'NTNU - Norwegian University of Science and Technology',
-    'NTNU University Museum'
-]
-
-UiB_list = [
-    'University of Bergen',
-    'CENTRE FOR GEOBIOLOGY, DEPARTMENT OF BIOLOGY, UNIVERSITY OF BERGEN, NORWAY',
-    'Universitetet i Bergen',
-    'UNIVERSITY OF BERGEN/DEPT. OF BIOLOGY',
-    'K.G. Jebsen center for deep-sea research, University of Bergen',
-    'Center for Geobiology, University of Bergen',
-    'UNIVERSITY OF BERGEN',
-    'UiB'
-]
-
-NMBU_list = [
-    'FACULTY OF CHEMISTRY, BIOTECHNOLOGY AND FOOD SCIENCE (IKBM), NORWEGIAN UNIVERSITY OF LIFE SCIENCES (NMBU), NORWAY',
-    'NMBU Norwegian University of Life Sciences',
-    'NMBU',
-    'Norwegian University of Life Sciences (NMBU)',
-]
-
-UiT_list = [
-    'University of Tromso',
-    'UiT the Arctic University of Norway',
-    'UiT: The Arctic University of Norway',
-    'UiT, The Arctic University of Norway',
-    'UIT',
-    'UiT The Arctic University if Norway',
-    'UiT The Arctic University',
-    'UiT - The Arctic University of Norway',
-    'The Arctic University of Norway',
-    'University of Tromsoe',
-    'Sletvold H., Department of Pharmacy, University of Tromso, Tromso, N-9037, NORWAY',
-    'Tromsoe University Museum',
-    'University of Tromso - The Arctic University of Norway',
-    'UNIVERSITY OF TROMSO'
-]
-
-FHI_list=[
-    'Norwegian Institute of Public Health (NIPH)',
-    'NIPH',
-    'NORWEGIAN INSTITUTE OF PUBLIC HEALTH',
-    'Folkehelseinstituttet (FHI), Norway'
-]
-
-centres = {
-    "UiO": UiO_list,
-    "UiB": UiB_list,
-    "UiT": UiT_list,
-    "NMBU": NMBU_list,
-    "NTNU": NTNU_list,
-    "FHI": FHI_list
-}
-
-for centre in centres.keys(): 
-    fields.loc[fields["center_name"].isin(centres[centre]), "center_name"] = centre
 
 # Grouping the data to plot into a unique dataframe, setting to 0. entries lacking data
 data2plot={}
